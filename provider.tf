d92e4ba8f5cf15404782f5e531d29236181b1ee8
Aashish Verma <67453059+aashishverma-git@users.noreply.github.com>
2023-02-24 13:18:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,19 +1,16 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.40.0"
-    }
-    random = {
-      source  = "hashicorp/random" # create random string or unique name for your resources     
-      version = "~>3.0"
+  backend "remote" {
+    organization = "myorg23"
+
+    workspaces {
+      name = "aashish_cli"
     }
   }
 }
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
+  features {}
   subscription_id = "f8657aca-ec75-4250-af28-7b4657f1484a"
   tenant_id       = "d5ef9e2e-cc94-4a25-a1cb-0113d6fdb95d"
-  features {}
+  client_secret   = "X408Q~pP6NEaoj7p20_b9B8YW.oKYs.GNd.MJcqg"
+  client_id       = "e11f80ac-8d66-4b69-bfb6-308ec503328d"
 }
