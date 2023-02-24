7b00d498f1e0266e97a1bc040e28a24c1148c8d7
Aashish Verma <67453059+aashishverma-git@users.noreply.github.com>
2023-02-24 13:54:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,8 +9,8 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  subscription_id = "f8657aca-ec75-4250-af28-7b4657f1484a"
-  tenant_id       = "d5ef9e2e-cc94-4a25-a1cb-0113d6fdb95d"
-  client_secret   = "X408Q~pP6NEaoj7p20_b9B8YW.oKYs.GNd.MJcqg"
-  client_id       = "e11f80ac-8d66-4b69-bfb6-308ec503328d"
+#   subscription_id = "f8657aca-ec75-4250-af28-7b4657f1484a"
+#   tenant_id       = "d5ef9e2e-cc94-4a25-a1cb-0113d6fdb95d"
+#   client_secret   = "X408Q~pP6NEaoj7p20_b9B8YW.oKYs.GNd.MJcqg"
+#   client_id       = "e11f80ac-8d66-4b69-bfb6-308ec503328d"
 }
